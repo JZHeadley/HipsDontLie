d0a7722d3a6ec093f3fd97eee18d6bfdbce81f90
jzheadley <headleyjz@vcu.edu>
2019-04-13 22:22:42 -0400

    Added an app router so we can have multiple pages.

---
+++ settings/SettingsComponent.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+
+
+class SettingsComponent extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    componentDidMount() {
+    }
+    componentWillUnmount() {
+    }
+
+    render() {
+        return (
+            <View>
+              <Text>Settings Page</Text>
+            </View>
+        )
+    }
+}
+export default SettingsComponent;
